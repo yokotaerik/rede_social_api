bde8aac5f5ff000667e45ef89483c2c8c4d91226
yokotaerik <yokota082003@gmail.com>
2024-01-01 23:11:48 -0300

    Criando validacao de registro e login, controlando os possiveis erros

--- src/main/java/com/example/blog/repositories/UserRepository.java
+++ src/main/java/com/example/blog/repositories/UserRepository.java
@@ -8,4 +8,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface UserRepository extends MongoRepository<User, String> {
     UserDetails findByUsername(String username);
+
+    User findByEmail(String email);
 }
